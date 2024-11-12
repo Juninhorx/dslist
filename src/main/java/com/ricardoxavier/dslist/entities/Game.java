4e000bc2b7d49243e1277285d49c21c317d37d17
Ricardo Xavier <rxc.projects@gmail.com>
2024-11-11 22:43:35 -0300

    Game seed

--- src/main/java/com/ricardoxavier/dslist/entities/Game.java
+++ src/main/java/com/ricardoxavier/dslist/entities/Game.java
@@ -23,22 +23,28 @@ public class Game {
 								// atributo)
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT") // seta o tipo do campo para TEXT para suportar texto granda quando o JPA criar o BD
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -77,11 +83,11 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
 	}
 
 	public String getImgUrl() {
@@ -109,6 +115,14 @@ public class Game {
 	}
 
 	// Métodos para comparar dois games (iguais ou não).
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
 
 	@Override
 	public int hashCode() {
