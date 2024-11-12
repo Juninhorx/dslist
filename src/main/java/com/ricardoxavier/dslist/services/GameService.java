e263f626c667b110627f7ee817401834521af209
Ricardo Xavier <rxc.projects@gmail.com>
2024-11-12 19:53:00 -0300

    GET game by id

--- src/main/java/com/ricardoxavier/dslist/services/GameService.java
+++ src/main/java/com/ricardoxavier/dslist/services/GameService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ricardoxavier.dslist.dto.GameDTO;
 import com.ricardoxavier.dslist.dto.GameMinDTO;
 import com.ricardoxavier.dslist.entities.Game;
 import com.ricardoxavier.dslist.repositories.GameRepository;
+
 
 @Service // Injetando componentes -> mostrando para o sistema que é um serviço
 public class GameService {
@@ -16,6 +19,13 @@ public class GameService {
 	private GameRepository gameRepository; // Instanciando o repositorio para usar (injetando)
 	                                       // Sempre um componente vai chamar o outro
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
